a64e3e3542532a072ff02e9f5d01b1be805b01b2
boatman3132 <boatman3132@gmail.com>
2024-12-13 22:23:46 +0800

    快速更改心跳RPM

--- src/main.cpp
+++ src/main.cpp
@@ -8,41 +8,56 @@ const int stepsPerRevolution = 2048; // 28BYJ-48 的步數
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 int stepsToMove = 0;
 
+
+int heart_rate = 60; //RPM
+
+
+
 void setup() {
   // 初始化串列通訊，設定波特率為9600
   Serial.begin(9600);
   // 設定馬達速度（每分鐘轉速）
-  myStepper.setSpeed(5); // 5 RPM
+  myStepper.setSpeed(1); // 5 RPM
 }
+
+
 
 
 void rotateMotor() {
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis % 1000 >= 0 && currentMillis % 1000 <= 100) {
-    // 讓馬達正向旋轉一定的步數
-    stepsToMove += stepsPerRevolution * 0.002; // 計算要移動的步數
-    
-  }
 
-  if (currentMillis % 1000 >= 800 && currentMillis % 1000 <= 900) {
-    // 讓馬達正向旋轉一定的步數
-    stepsToMove += -stepsPerRevolution * 0.002; // 計算要移動的步數
-    
-  }
 
-    if (currentMillis % 5000 >= 0 && currentMillis % 5000 <= 2000) {
+// 心跳
+  if (currentMillis % (60000/heart_rate) >= 0 && currentMillis % (60000/heart_rate) <= 200) {
     // 讓馬達正向旋轉一定的步數
     stepsToMove += 1; // 計算要移動的步數
     
   }
 
-  if (currentMillis % 5000 >= 2500 && currentMillis % 5000 <= 4500) {
+  if (currentMillis % (60000/heart_rate) >= (60000/heart_rate - 200) ) {
     // 讓馬達正向旋轉一定的步數
-    stepsToMove += -1; // 計算要移動的步數
+    stepsToMove -= 1; // 計算要移動的步數
     
   }
+
+
+
+
+// 呼吸
+
+  //   if (currentMillis % 6000 >= 0 && currentMillis % 6000 <= 1000) {
+  //   // 讓馬達正向旋轉一定的步數
+  //   stepsToMove += 1; // 計算要移動的步數
+    
+  // }
+
+  // if (currentMillis % 6000 >= 2500 && currentMillis % 6000 <= 3500) {
+  //   // 讓馬達正向旋轉一定的步數
+  //   stepsToMove += -1; // 計算要移動的步數
+    
+  // }
 
   myStepper.step(stepsToMove);
   stepsToMove = 0;
