9543ab92926caa17c078789198c82c6a02ed134e
Louis Hsueh <boatman3132@gmail.com>
2024-12-15 16:25:19 +0800

    舵機放在書擋後面震動

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ Servo myServo;  // 宣告舵機物件
 // 選擇 GPIO 21 作為舵機的信號腳位
 #define SERVO_PIN 21
 
-int heart_rate = 60; //RPM
+int heart_rate = 120; //RPM
 
 void setup() {
   Serial.begin(115200);
@@ -18,13 +18,13 @@ void setup() {
 
 void loop() {
   // 範例：讓舵機從 0 度移動到 180 度
-  for (int pos = 0; pos <= 3; pos += 1) {
+  for (int pos = 45; pos <= 50; pos += 1) {
     myServo.write(pos);  // 設定舵機位置
     delay(15);           // 確保舵機有足夠時間移動
   }
 
   // 再讓舵機從 180 度移動回 0 度
-  for (int pos = 3; pos >= 0; pos -= 1) {
+  for (int pos = 50; pos >= 45; pos -= 1) {
     myServo.write(pos);
     delay(15);
   }
