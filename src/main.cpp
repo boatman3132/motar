fdb7705e54d39eb29f0ce4e66aa7e5f45bc9dffe
boatman3132 <boatman3132@gmail.com>
2024-12-16 11:09:04 +0800

    直觀化修改舵機模擬心跳的參數

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,36 @@
 
 Servo myServo;  // 宣告舵機物件
 
-// 選擇 GPIO 21 作為舵機的信號腳位
-#define SERVO_PIN 21
+// 選擇 GPIO 1 作為舵機的信號腳位
+#define SERVO_PIN 1
 
-int heart_rate = 120; //RPM
+int heart_rate = 60; //RPM
+
+// 舵機參數
+int servo_start_angle = 45; //旋轉起始角度
+int servo_rotate_angle = 5; //旋轉角度
+int servo_rotate_time = 15; //旋轉時間（ms）
+
 
 void setup() {
   Serial.begin(115200);
   
-  // 初始化舵機，連接到 GPIO 21
+  // 初始化舵機，連接到 GPIO 1
   myServo.attach(SERVO_PIN);
 
 }
 
 void loop() {
   // 範例：讓舵機從 0 度移動到 180 度
-  for (int pos = 45; pos <= 50; pos += 1) {
+  for (int pos = servo_start_angle ; pos <= (servo_start_angle + servo_rotate_angle) ; pos += 1) {
     myServo.write(pos);  // 設定舵機位置
-    delay(15);           // 確保舵機有足夠時間移動
+    delay(servo_rotate_time);           // 確保舵機有足夠時間移動
   }
 
   // 再讓舵機從 180 度移動回 0 度
-  for (int pos = 50; pos >= 45; pos -= 1) {
+  for (int pos = servo_start_angle + servo_rotate_angle; pos >= servo_start_angle; pos -= 1) {
     myServo.write(pos);
-    delay(15);
+    delay(servo_rotate_time);
   }
-  delay(60000/heart_rate-15*6);
+  delay(60000/heart_rate - servo_rotate_time * 2 * servo_rotate_angle);
 }
